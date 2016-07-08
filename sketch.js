bd92c0002eb5372d4ba9fc21b21e5a84410d08ac
Jeff Chin <jchin89@gmail.com>
2016-07-07 22:09:34 -0400

    read table

--- sketch.js
+++ sketch.js
@@ -1,9 +1,29 @@
   var playing = false;
   var fingers;
   var button;
-
+  
+function preload() {
+  //my table is comma separated value "csv"
+  //and has a header specifying the columns labels
+  table = loadTable("data/GPScombined.csv", "csv", "header");
+  //the file can be remote
+  //table = loadTable("http://p5js.org/reference/assets/mammals.csv",
+  //                  "csv", "header");
+}
 function setup() {
   createCanvas(640, 480);
+  
+  print(table.getRowCount() + " total rows in table");
+  print(table.getColumnCount() + " total columns in table");
+
+  print(table.getColumn("lat"));
+  //["Goat", "Leopard", "Zebra"]
+
+  //cycle through the table
+  for (var r = 0; r < table.getRowCount(); r++)
+    for (var c = 0; c < table.getColumnCount(); c++) {
+      print(table.getString(r, c));
+    }
 
 }
 
